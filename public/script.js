2257389fd1efc2b0130e4b7329e5dc5cea3d8759
zigbee.sh@gmail.com <zigbee.sh@gmail.com>
2021-03-27 18:32:57 +0530

    updated the player

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Connection to server
-var socket = io.connect("http://movieserver-env.eba-btgpyma3.us-east-1.elasticbeanstalk.com/", {
+var socket = io.connect("http://localhost:3000", {
     reconnection: true
 });
 
@@ -9,38 +9,58 @@ socket.on('connect', function () {
     })
 
     socket.on('vid-state',state=>{
-        console.log(state);
-        if(state == 1){
+        if(vid.paused){
             vid.play();
-        }
-        else if(state == 0){
+         }
+         else {
             vid.pause();
-        }
+         }
+    })
+
+    socket.on('progress-bar-clicked',newTime => {
+        vid.currentTime = newTime;
     })
 
 });
 
 // Client side
 const vid = document.getElementById('videoPlayer');
-const videoDiv = document.getElementById('videoDiv');
+const playPauseBtn = document.getElementById('play-pause');
+const progress = document.getElementById('progress-bar');
+const fullscreenBtn = document.getElementById('fullscreen');
+const container = document.getElementById('container');
 
-
+fullscreenBtn.addEventListener('click',openFullscreen);
+playPauseBtn.addEventListener('click',pauseOrstart);
 vid.addEventListener('click',pauseOrstart);
 
-// 0 refers to pause, 1 refers to start
-let state = 0;
-
+vid.addEventListener('timeupdate', function() {
+    var progressBar = document.getElementById('progress-bar');
+    var percentage = Math.floor((100 / vid.duration) * vid.currentTime);
+    progressBar.value = percentage;
+    progressBar.innerHTML = percentage + '% played';
+ });
+ 
+ 
+ progress.addEventListener('click', function(e) {
+    var pos = (e.pageX  - this.offsetLeft) / this.offsetWidth;
+    var newTime =pos * vid.duration;
+    socket.emit('progress-bar-clicked',newTime);
+ });
 
 function pauseOrstart(){
-    if(state==0){
-        state = 1;
-    }
-    else if(state == 1){
-        state = 0;
-    }
-    socket.emit('vid-state',state);
+    socket.emit('vid-state','clicked');
 }
 
+function openFullscreen(){
+    if (container.requestFullscreen) {
+        container.requestFullscreen();
+      } else if (container.webkitRequestFullscreen) { /* Safari */
+        container.webkitRequestFullscreen();
+      } else if (container.msRequestFullscreen) { /* IE11 */
+        container.msRequestFullscreen();
+      }
+}
 
 
 
