ff5e6b2d0efa8947cd5fd4797da37c235e06d4a0
zigbee.sh@gmail.com <zigbee.sh@gmail.com>
2021-03-30 22:39:56 +0530

    made changes to video player

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Connection to server
-var socket = io.connect("http://localhost:3000", {
+var socket = io.connect("http://movieserver-env.eba-r93zfwia.us-east-1.elasticbeanstalk.com/", {
     reconnection: true
 });
 
@@ -11,9 +11,11 @@ socket.on('connect', function () {
     socket.on('vid-state',state=>{
         if(vid.paused){
             vid.play();
+            playPauseBtn.innerHTML = "&#9612;&#9612;";
          }
          else {
             vid.pause();
+            playPauseBtn.innerHTML = "&#9658";
          }
     })
 
@@ -29,17 +31,17 @@ const playPauseBtn = document.getElementById('play-pause');
 const progress = document.getElementById('progress-bar');
 const fullscreenBtn = document.getElementById('fullscreen');
 const container = document.getElementById('container');
+const volinc = document.getElementById('volinc');
+const voldec = document.getElementById('voldec');
+
+
+
 
 fullscreenBtn.addEventListener('click',openFullscreen);
 playPauseBtn.addEventListener('click',pauseOrstart);
-vid.addEventListener('click',pauseOrstart);
 
-vid.addEventListener('timeupdate', function() {
-    var progressBar = document.getElementById('progress-bar');
-    var percentage = Math.floor((100 / vid.duration) * vid.currentTime);
-    progressBar.value = percentage;
-    progressBar.innerHTML = percentage + '% played';
- });
+
+vid.addEventListener('timeupdate', updateProgressBar);
  
  
  progress.addEventListener('click', function(e) {
@@ -47,6 +49,21 @@ vid.addEventListener('timeupdate', function() {
     var newTime =pos * vid.duration;
     socket.emit('progress-bar-clicked',newTime);
  });
+
+ volinc.addEventListener('click', function(e) {
+    alterVolume('+');
+ });
+ 
+ 
+ voldec.addEventListener('click', function(e) {
+    alterVolume('-');
+ });
+
+ 
+
+
+
+ //Functions
 
 function pauseOrstart(){
     socket.emit('vid-state','clicked');
@@ -62,5 +79,19 @@ function openFullscreen(){
       }
 }
 
+function updateProgressBar(){
+    var progressBar = document.getElementById('progress-bar');
+    var percentage = Math.floor((100 / vid.duration) * vid.currentTime);
+    progressBar.value = percentage;
+};
 
+function alterVolume(dir){
+    var currentVolume = Math.floor(vid.volume * 10) / 10;
+   if (dir === '+') {
+      if (currentVolume < 1) vid.volume += 0.1;
+   }
+   else if (dir === '-') {
+      if (currentVolume > 0) vid.volume -= 0.1;
+   }
+}
 
